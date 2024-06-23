d188c8ed5ac4d10887ce2bdf9fe3e3523063b467
Yirang Kim <olrang.2@gmail.com>
2024-06-23 22:34:39 +0900

    🎉 Feat: "VectorDB 테스트"

--- VectorDB/main.py
+++ VectorDB/main.py
@@ -4,7 +4,7 @@ def main():
     """
     메인 함수
     """
-    print("Hello, World!")  # "Hello, World!"를 출력합니다
+    print("Hello, World!")  # "Hello, World!"를 출력합니다!
 
 if __name__ == "__main__":
     main()
