9c2fcef951c68f2a39073c91d58f483d32ee2551
DESKTOP-0L11UB0\user <olrang.2@gmail.com>
2024-07-11 18:10:59 +0900

    ⚡️ Feat:"답변 prompt적용"

--- VectorDB/main.py
+++ VectorDB/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
 # from upload import upload_router
 from upload_v2 import upload_router_v2
+from upload_v4 import upload_router_v4
 #from upload_json import upload_json_router
 # from query import query_router
 from query_v2 import query_router_v2
 from query_v3 import query_router_v3
+from query_v4 import query_router_v4
 #from query_lang import query_router_langch
 
 app = FastAPI()
 
 #app.include_router(upload_router, prefix="/api") # VDB저장
 app.include_router(upload_router_v2, prefix="/api") # chunck VDB저장
+app.include_router(upload_router_v4, prefix="/api") # VDB저장
 #app.include_router(query_router, prefix="/api") # RAG
 app.include_router(query_router_v2, prefix="/api") # RAG
 app.include_router(query_router_v3, prefix="/api") # RAG
+app.include_router(query_router_v4, prefix="/api") # RAG
 #app.include_router(query_router_langch, prefix="/api") # query_router_langch
 #app.include_router(upload_json_router, prefix="/api") # json
 
